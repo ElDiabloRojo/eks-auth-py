776d6629f99eb361c26de2a760275ea53492f921
ElDiabloRojo <holdens.uk@googlemail.com>
2020-04-11 13:00:31 +0100

    project config implemented

--- aws/client.py
+++ aws/client.py
@@ -1,15 +1,12 @@
-import boto3
-from tools import config
 
-project_config = config.main()
 
 class AWSCLient:
-    def __init__(self, service, access_key, secret_key, session_token=None):
+    def __init__(self, service, access_key, secret_key, region, session_token=None):
         self.service = service
         self.access_key = access_key
         self.secret_access_key = secret_key
         self.session_token = session_token
-        self.region = project_config.get('REGION')
+        self.region = region
 
     def show_object(self):
         print(self.__dict__)
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from profile import selector, profile
 from aws import client
+import configparser
+
+
+def config():
+    config = configparser.ConfigParser()
+    config.read('./config.ini')
+
+    return config
 
 
 def main():
+    project_config = config()
+    region = project_config.get('general', 'REGION')
     current_profile = profile.Profile(selector.main())
     current_profile.get_aws_keys()
-    iam_client = client.AWSCLient(service='iam', access_key=current_profile.access_key, secret_key=current_profile.secret_key)
+    iam_client = client.AWSCLient(service='iam', access_key=current_profile.access_key, secret_key=current_profile.secret_key, region=region)
     client.AWSCLient.show_object(iam_client)
+
 
 if __name__ == '__main__':
     main()
